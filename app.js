f8648f20e85ee974b019db3a25a16d315d362ce4
samndambuki <sam.ndambuki08@gmail.com>
2023-08-13 22:34:41 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+
+import chalk from 'chalk'
+import express from 'express'
+import debug from 'debug'
+import morgan from 'morgan';
+import path from 'path'
+
+// const express = require('express')
+
+// const chalk = require('chalk')
+
+const __filename = new URL(import.meta.url).pathname;
+const __dirname = path.dirname(__filename)
+
+const debugLogger = debug('app')
+
+const app = express();
+
+app.use(morgan('combined'))
+app.use(express.static(path.join(__dirname,'public')))
+
+app.get('/',(req,res)=>{
+    res.send("Hello Sam");
+})
+
+app.listen(3000,()=>{
+    debug(`listening on port ${chalk.green('3000')}`)
+})
